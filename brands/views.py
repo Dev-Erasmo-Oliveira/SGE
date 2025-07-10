71feb2944c8f78fd3961808884229c1db831056b
Erasmo Oliveira <erasmo.oliveira@redwolfai.com.br>
2025-07-10 12:07:38 -0300

    Concluída todo o CRUD da app de BRANDS, com a estilização e criação de botões via formulário para fazer as validações

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView,DeleteView
 from . import models, forms
 
 class BrandListView(ListView):
@@ -33,4 +33,10 @@ class BrandUpdateView(UpdateView):
     template_name = 'brand_update.html'
     form_class = forms.BrandForm
     success_url = reverse_lazy('brand_list')
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
+    success_url = reverse_lazy('brand_list')
+
     
