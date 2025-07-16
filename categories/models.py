390d4a17cc1495956cda453bb9004ff6b8812258
Erasmo Oliveira <erasmohazzagui021@gmail.com>
2025-07-15 21:54:12 -0300

    Alteração na quantidade

--- categories/models.py
+++ categories/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
